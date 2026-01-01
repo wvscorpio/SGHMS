bb99a61634d16a75042df537f7cea97323441bf2
wvscorpio <aqilahsairi0310@gmail.com>
2026-01-02 00:38:23 +0800

    Connect Database

---
+++ db/dbcon.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = "re_sghms";
+$dbuser = 'root';
+$dbpass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
