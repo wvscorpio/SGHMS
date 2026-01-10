f14944b24b5557e2e5d8a63228c27a7e03500f44
wvscorpio <aqilahsairi0310@gmail.com>
2026-01-10 20:53:43 +0800

    Anis Update Toggle Password Only

--- auth/register.php
+++ auth/register.php
@@ -107,14 +107,7 @@ if (isset($_POST['register'])) {
             <label>Password</label>
             <div class="password-wrapper">
                 <input type="password" name="password" id="password" required>
-                <button type="button" class="toggle-password" onclick="togglePassword()">
-                    <svg class="eye-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path class="eye-open" d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                        <circle class="eye-open" cx="12" cy="12" r="3"></circle>
-                        <path class="eye-closed" d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
-                        <line class="eye-closed" x1="1" y1="1" x2="23" y2="23"></line>
-                    </svg>
-                </button>
+                <button type="button" class="toggle-password" onclick="togglePassword()"></button>
             </div>
 
             <button type="submit" name="register" class="action-btn">
