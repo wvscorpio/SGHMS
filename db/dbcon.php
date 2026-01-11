e7b3777c75b853456d0efaeb264123458e3380d9
wvscorpio <aqilahsairi0310@gmail.com>
2026-01-11 18:20:23 +0800

    DB

--- db/dbcon.php
+++ db/dbcon.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = ""; 
 $dbname = "re_sghms"; 
 
-$conn = new mysqli($servername, $username, $password, $dbname,3307); 
+$conn = new mysqli($servername, $username, $password, $dbname); 
 if($conn->connect_error){ 
     die("Connection failed: ". $conn->connect_error); 
 } 
